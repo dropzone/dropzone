fea3e5918a54a80c5384fbd4266d70b6c874030d
Matias Meno <m@tias.me>
2013-01-23 15:13:17 +0100

    Upgrading version to 1.2.6 (Opera12 for windows is now working, and reenabled IE10.)

--- downloads/dropzone.js
+++ downloads/dropzone.js
@@ -410,7 +410,7 @@ require.register("dropzone/lib/dropzone.js", function(exports, require, module){
 
     __extends(Dropzone, _super);
 
-    Dropzone.prototype.version = "1.2.6-dev";
+    Dropzone.prototype.version = "1.2.6";
 
     /*
       This is a list of all available events you can register on a dropzone object.
