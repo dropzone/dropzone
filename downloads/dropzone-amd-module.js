8178b84cfc5ca4c5b59d09129fe595ad8cd8e5cd
Matias Meno <m@tias.me>
2013-02-06 01:35:34 +0100

    Upgrading version to 1.3.4

--- downloads/dropzone-amd-module.js
+++ downloads/dropzone-amd-module.js
@@ -201,7 +201,7 @@ Emitter.prototype.hasListeners = function(event){
 
     __extends(Dropzone, _super);
 
-    Dropzone.prototype.version = "1.3.4-dev";
+    Dropzone.prototype.version = "1.3.4";
 
     /*
       This is a list of all available events you can register on a dropzone object.
