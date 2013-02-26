67cc54621c01101504168227d51e912e54f48759
Matias Meno <m@tias.me>
2013-02-26 20:49:44 +0100

    Upgrading version to 1.3.8-dev

--- downloads/dropzone.js
+++ downloads/dropzone.js
@@ -411,7 +411,7 @@ require.register("dropzone/lib/dropzone.js", function(exports, require, module){
 
     __extends(Dropzone, _super);
 
-    Dropzone.prototype.version = "1.3.7";
+    Dropzone.prototype.version = "1.3.8-dev";
 
     /*
       This is a list of all available events you can register on a dropzone object.
