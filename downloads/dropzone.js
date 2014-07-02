3cec515d2a6c336b4f39a327242b9b610e43142a
n0wak <mnowak+git@gmail.com>
2014-07-02 12:27:22 +0100

    Do not send null input name attributes on xhr send
    Prevent adding formData to XHR with a null key. This happens when the
    input attribute on a form is unset and causes an error.
    
    I needed to do this because the submit button was being added to the
    xhr formData (Amazon S3 can restrict requests to specific header and
    this wasn't specified) and removing the name attribute from the submit
    button sent a 'null' key in post.

--- downloads/dropzone.js
+++ downloads/dropzone.js
@@ -55,7 +55,7 @@ require.define = function (name, exports) {
     exports: exports
   };
 };
-require.register("component~emitter@1.1.2", function (exports, module) {
+require.register("component~emitter@1.1.3", function (exports, module) {
 
 /**
  * Expose `Emitter`.
@@ -267,7 +267,7 @@ require.register("dropzone/lib/dropzone.js", function (exports, module) {
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
     __slice = [].slice;
 
-  Em = typeof Emitter !== "undefined" && Emitter !== null ? Emitter : require("component~emitter@1.1.2");
+  Em = typeof Emitter !== "undefined" && Emitter !== null ? Emitter : require("component~emitter@1.1.3");
 
   noop = function() {};
 
@@ -1471,6 +1471,8 @@ require.register("dropzone/lib/dropzone.js", function (exports, module) {
                 formData.append(inputName, option.value);
               }
             }
+          } else if (inputName === null) {
+
           } else if (!inputType || ((_ref4 = inputType.toLowerCase()) !== "checkbox" && _ref4 !== "radio") || input.checked) {
             formData.append(inputName, input.value);
           }
